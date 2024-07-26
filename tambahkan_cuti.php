a4a8d7fddbc225c99ccc1f3b4d7e540921b44bc9
ermayanti88 <eermayantiii@gmail.com>
2024-07-26 13:36:06 +0800

    ubah

--- tambahkan_cuti.php
+++ tambahkan_cuti.php
@@ -8,6 +8,8 @@ $id = $_POST['id_pegawai'];
 $datax = mysqli_query($conn, "select * from pegawai where id_pegawai='$id'");
 $result = mysqli_fetch_array($datax);
 $nama = $result['nama_pegawai'];
+var_dump($nama);
+die;
 
 
 
